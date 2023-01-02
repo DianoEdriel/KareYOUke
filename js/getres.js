5436fd9deecb4bcf4d162e95ab72f97074f2ba25
DianoEdriel <dianoedrieljohn.ed@gmail.com>
2023-01-02 22:35:10 +0800

    Update getres.js

--- js/getres.js
+++ js/getres.js
@@ -1,6 +1,8 @@
 function searchvid(){
-$.getJSON("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=unholy%20acoustic%20karaoke&key=AIzaSyAIVusd82WaGoihJTZ-9-52K2NAt4gSHYU" function(data){
-    console.log(data)
+console.log("hello")
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=unholy%20acoustic%20karaoke&key=AIzaSyAIVusd82WaGoihJTZ-9-52K2NAt4gSHYU")
+.then((res)=>{
+    console.log(res.json())
 })
    
 }
